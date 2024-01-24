4145115576c059db744c4bfa9ba8247627349749
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-01-24 17:15:41 +0100

    refactor: replace antd carousel with custom, remove antd library

--- components/Main-carousel/MainCarousel.tsx
+++ components/Main-carousel/MainCarousel.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import Image from 'next/image';
-import { Carousel } from 'antd';
-import carouselImages from '../../configs/carousel.config';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 import styles from './_mainCarousel.module.scss';
+import carouselImages from '../../configs/carousel.config';
 
 function MainCarousel() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === carouselImages.length - 1 ? 0 : prevIndex + 1,
+      );
+    }, 3000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div className={styles.carousel__wrapper}>
-      <Carousel autoplay dots={false} effect="fade" className={styles.carousel}>
-        {carouselImages.map((image) => (
-          <div>
-            <Image
-              src={image}
-              className={styles.carousel__image}
-              key={image.src}
-              alt="Grota solna"
-            />
-          </div>
-        ))}
-      </Carousel>
+    <div className={styles.carousel}>
+      {carouselImages.map((image, index) => (
+        <div
+          key={image}
+          className={styles.carousel__image}
+          style={{
+            backgroundImage: `url(${image})`,
+            opacity: index === currentIndex ? 1 : 0,
+          }}
+        />
+      ))}
     </div>
   );
 }
